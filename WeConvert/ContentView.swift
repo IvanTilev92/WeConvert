a5d6fac6e7e4b23fe50f73c7de18c8dc98736c57
IvanTilev <iivantilev@gmail.com>
2023-09-17 13:32:51 +0300

    Initial Commit

---
+++ WeConvert/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeConvert
+//
+//  Created by Ivan Tilev on 17.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Ivan Tilev on 17.09.23.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
