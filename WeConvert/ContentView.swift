e36f9f5ebe8b54d5707693f106812527489c27a7
IvanTilev <iivantilev@gmail.com>
2023-09-17 14:05:21 +0300

    Creating the user inout find that takes a value and the picker that let them user choose from witch metric to convert from

--- WeConvert/ContentView.swift
+++ WeConvert/ContentView.swift
@@ -4,13 +4,38 @@
 //
 //  Created by Ivan Tilev on 17.09.23.
 //
+// This is a simple app that convers Temperature values from Celsius, Fahrenheit or Kelvin
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userInput: String = ""
+    @State private var chosenUnit: String = ""
+    @State private var userOutput: String = ""
+    
+    // The temperure metrics that the user can choose from
+    let metricValues = ["Celsius", "Fahrenheit", "Kelvin"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    // TextField that take user input
+                    TextField("Type a value to convert", text: $userInput)
+                        .keyboardType(.numberPad)
+                    
+                    // Segmented control that the user can choose from what unit to convert from
+                    Picker("Conver from: ", selection: $chosenUnit) {
+                        ForEach(metricValues, id: \.self) {
+                            Text($0)
+                        }
+                    }.pickerStyle(.segmented)
+                }
+            }.navigationTitle("TempConvert")
+
+        }
+        // Segmented control that the user can choose what type of unit to convert to
+        // TextView that shows the converted input
     }
 }
 
